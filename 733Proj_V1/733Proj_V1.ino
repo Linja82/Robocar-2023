d5bd9a9c8ce7fbed2ac0a3e1f68977d3b8265b55
Alec <52588950+Linja82@users.noreply.github.com>
2023-03-08 02:10:46 -0500

    Added pin assignments

--- 733Proj_V1/733Proj_V1.ino
+++ 733Proj_V1/733Proj_V1.ino
@@ -1,9 +1,38 @@
+#define PIN_Left_LineTracker    A2
+#define PIN_Middle_LineTracker  A1
+#define PIN_Right_LineTracker   A0
+
+#define PIN_Left_Motor          6
+#define PIN_Right_Motor         5
+
+#define PIN_Servo1              10
+#define PIN_Servo2              11
+
+#define PIN_MPU6050_SCL         A5
+#define PIN_MPU6050_SDA         A4
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  
+  pinMode(PIN_Left_LineTracker, INPUT);
+  pinMode(PIN_Middle_LineTracker, INPUT);
+  pinMode(PIN_Right_LineTracker, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void lineFollow()
+{
+  /* Line follow pseudo code
+   * If (left line tracker see dark)
+   *    More speed to left drive motor
+   * else if (right line tracker see dark)
+   *    More speed to right drive motor
+   * else
+   *    Equal speed to left and right drive motor
+   */
 }
