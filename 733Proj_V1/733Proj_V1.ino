4923b9c3ba9f3bab8a2721ce1bf72c55429d52fc
Alec <52588950+Linja82@users.noreply.github.com>
2023-03-09 13:34:06 -0500

    Added more pseudo code and initializations

--- 733Proj_V1/733Proj_V1.ino
+++ 733Proj_V1/733Proj_V1.ino
@@ -1,3 +1,13 @@
+/////////////////////////////////Notes///////////////////////////////////////////
+
+// 
+
+////////////////////////////////Imports//////////////////////////////////////////
+
+#include <Servo.h>
+
+////////////////////////////Pin Assignments//////////////////////////////////////
+
 #define PIN_Right_LineTracker   A0
 #define PIN_Middle_LineTracker  A1
 #define PIN_Left_LineTracker    A2
@@ -16,28 +26,57 @@
 #define PIN_Ultrasonic_Echo     12
 #define PIN_Ultrasonic_Trigger  13
 
+///////////////////////////Servo Definitions/////////////////////////////////////
+
+Servo panServo;
+
+/////////////////////////////Code be here////////////////////////////////////////
+
+int main(void)
+{
+  setup();
+  lineFollow();
+  maze();
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   
+  // Attach pan servo
+  panServo.attach(PIN_Servo1);
+
+  // Line tracker Pin Modes
   pinMode(PIN_Left_LineTracker, INPUT);
   pinMode(PIN_Middle_LineTracker, INPUT);
   pinMode(PIN_Right_LineTracker, INPUT);
+
+  // Motor driver Pin Modes
+  pinMode(PIN_Right_Motor, OUTPUT);
+  pinMode(PIN_Left_Motor, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-
-void lineFollow()
-{
+void lineFollow(){
   /* Line follow pseudo code
-   * If (left line tracker see dark)
+   * If (All line trackers see dark)
+   *    Intersection detected
+   *    Stop at intersection
+   *    delay(3500);
+   * else if (left line tracker see dark)
    *    More speed to left drive motor
    * else if (right line tracker see dark)
    *    More speed to right drive motor
    * else
    *    Equal speed to left and right drive motor
+   */
+}
+
+void maze(){
+  /* Maze traverse pseudo code
+   * Move forward 1 unit
+   * Scan left
+   * Scan middle
+   * Scan Right
+   * Rotate to whichever direction has the largest measured distance
    */
 }
