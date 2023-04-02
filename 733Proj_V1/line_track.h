988b528b55fff0b2a42a69a87d1b77c23ea310bd
Alec <52588950+Linja82@users.noreply.github.com>
2023-04-02 14:15:53 -0400

    line calibrate test

---
+++ 733Proj_V1/line_track.h
@@ -0,0 +1,7 @@
+void calibrate() {
+  int leftIR = analogRead(PIN_Left_LineTracker);
+  int middleIR = analogRead(PIN_Middle_LineTracker);
+  int rightIR = analogRead(PIN_Right_LineTracker);
+
+  
+}
---
+++ Line_Calibrate_V1/Line_Calibrate_V1.ino
@@ -0,0 +1,46 @@
+/////////////////////////// Pin Assignments /////////////////////////////////////
+
+#define PIN_Right_LineTracker   A0
+#define PIN_Middle_LineTracker  A1
+#define PIN_Left_LineTracker    A2
+
+#define PIN_VBat                A3
+
+#define PIN_MPU6050_SDA         A4
+#define PIN_MPU6050_SCL         A5
+
+#define PIN_Mode_Switch         2
+
+#define PIN_Motor_Standby       3
+
+#define PIN_LED_Data            4
+
+#define PIN_Right_Motor         5
+#define PIN_Left_Motor          6
+#define PIN_Motor_R_IN1         7
+#define PIN_Motor_L_IN1         8
+
+#define PIN_IR_Receiver         9
+
+#define PIN_Servo1              10
+#define PIN_Servo2              11
+
+#define PIN_Ultrasonic_Echo     12
+#define PIN_Ultrasonic_Trigger  13
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_Right_LineTracker, INPUT);
+  pinMode(PIN_Middle_LineTracker, INPUT);
+  pinMode(PIN_Left_LineTracker, INPUT);
+
+  pinMode(PIN_VBat, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(PIN_VBat)); 
+  delay(500);
+}
